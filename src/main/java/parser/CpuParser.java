151e1a09f6f8594d25a732aa384005924f10d6b6
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2023-03-30 09:43:37 +0530

    Add temp fix to floating issue

--- src/main/java/parser/CpuParser.java
+++ src/main/java/parser/CpuParser.java
@@ -19,7 +19,8 @@ public class CpuParser {
         try {
             ArrayList<String> skipList = new ArrayList<String>(Arrays.asList(skipString.split(",")));
 
-            skipList.add("$gen");
+//            skipList.add("$gen");
+
             skipList.add("getAnonType");
 
             String file = "CpuPre.json"; // File path of the Profiler Output json file
